7d4809aba9551a30b8ec29e45e775115965e5134
Mxles-Arm <ccooppto@gmail.com>
2026-02-25 22:12:30 +0700

    updata

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Header from './component/nav.jsx';
-import home from './component/home.jsx';
-import Browse from './component/Browse.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Playlist from './component/Playlist.jsx';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import Header from "./component/nav.jsx";
+import Home from "./component/home.jsx";
+import Browse from "./component/Browse.jsx";
+import Playlist from "./component/Playlist.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      npm install --save react-router-dom
-<BrowserRouter>
-      <Header />
+      <HashRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Navigate to="/home" replace />} />
-          <Route path="/home" element={<home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
           <Route path="/playlist" element={<Playlist />} />
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
